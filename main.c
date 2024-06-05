40b1a59b529f378c403b783d8da8f34ebe949e55
Seth Mazer <116219473+SethMazer@users.noreply.github.com>
2024-06-05 17:40:52 -0400

    Login Update
    I added a Login feature with a hard coded user ID and user Pin, aware its not secure, its not supposed to be. Also formatted stuff a lil easier with some separations and what not

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
+//Including libraries
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <stdbool.h>
-#include <gtk-4.0/gtk/gtk.h>
-#include <gtk-4.0/gtk/css/gtkcss.h>
-
 #define BUFFER_SIZE 64
 
 
+//------------------------
+// Bank action functions
+//-------------------------
+
+
+//Deposit function, simply adding an amount to the users balance
 void Deposit(int *userBalance){
 
     //Creating deposit variable
@@ -22,9 +24,9 @@ void Deposit(int *userBalance){
     *userBalance += depositAmount;
     printf("Your new balance is: %d", *userBalance);
 
-
 }
 
+//Same as Deposit function except subtracting an amount from the users balance.
 void Withdrawal(int *userBalance){
 
     //Creating withdrawal amount
@@ -40,29 +42,100 @@ void Withdrawal(int *userBalance){
 
 }
 
+//---------------------------
+// Main function
+//---------------------------
 
 int main (int argc, char *argv[]) {
 
-    //Creating a variable to store the users action and creating an initial balance
-    char userAction[BUFFER_SIZE];
+
+    //Adding user login info
+    int userID = 4068;
+    int userPin = 8163;
+    int userID_input;
+    int userPin_input;
+
+    //Creating user balance
     int userBalance = 10000;
 
-    //Asking the user what they would like to do, then receiving input
-    printf("Would you like to make a Withdrawal or Deposit? \n");
 
-    if(fgets(userAction, sizeof(userAction), stdin) != NULL){
+    //Welcoming User, asking for ID
+    printf("Welcome to the Bank of Seth, please enter your user ID: ");
+    scanf("%d", &userID_input);
+
+
+//-------------------------------------
+// Login conditionals
+//-------------------------------------
+
+
+    //Checking if ID exists, if it does continue, else state invalid and exit
+    if(userID_input == userID){
+        printf("User ID: %d \n", userID);
+        printf("Enter your pin:");
+        scanf("%d", &userPin_input);
+
+        //Checking if users pin input equals the correct  user pin, if so log user in and continue
+        if(userPin == userPin_input){
+            printf("Welcome, user %d \n", userID);
+            printf("Your current balance is %d: \n", userBalance);
+
+            //Stating PIN is invalid, and exiting
+        } else {
+            printf("Invalid PIN");
+            return 0;
+        }
+
+    //Stating id is invalid
+    } else {
+        printf("Invalid ID");
+        return 0;
+    }
+
+    //Emptying input buffer
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+
+
+
+//-------------------------------------
+// userAction conditionals
+//-------------------------------------
+
+
+    //Creating a variable to store the users action and creating an initial balance
+    char userAction[BUFFER_SIZE];
+
+
+    //Asking the user what they would like to do, then receiving input
+    printf("Would you like to Withdrawal, Deposit or Exit? \n");
+
+    if (fgets(userAction, sizeof(userAction), stdin) != NULL) {
         //Removing newline character
         userAction[strcspn(userAction, "\n")] = '\0';
+    } else {
+        printf("Error");
+        return 1;
     }
 
-
-    if(strcmp(userAction, "Deposit") == 0){
+    //If userAction is Deposit, call Deposit function, else call Withdrawal Function
+    if (strcmp(userAction, "Deposit") == 0) {
         Deposit(&userBalance);
 
-    } else if (strcmp(userAction, "Withdrawal") == 0){
+    } else if (strcmp(userAction, "Withdrawal") == 0) {
         Withdrawal(&userBalance);
 
+    } else if (strcmp(userAction, "Exit") == 0){
+        printf(":Exiting");
+        return 0;
+
+
+    } else {
+        printf("Invalid Option");
+        return 1;
     }
+
+
 
 return 0;
 
