ddc8012d933b0052832bce5553399f594fcf0afb
Seth Mazer <116219473+SethMazer@users.noreply.github.com>
2024-06-05 18:21:57 -0400

    Wallet Update
    Added a user wallet to make depositing and withdrawing more meaningful.  User can now add and subtract from his wallet, as well as exceed both their deposit amount, and withdrawal-able amounts.
    Added better functionality, now user just needs to type W, D, or E to do the three actions. Also added spacing between the terminal lines for easier reading.

--- main.c
+++ main.c
@@ -5,29 +5,40 @@
 #define BUFFER_SIZE 64
 
 
-//------------------------
+//-------------------------------
 // Bank action functions
-//-------------------------
+//--------------------------------
 
 
 //Deposit function, simply adding an amount to the users balance
-void Deposit(int *userBalance){
+void Deposit(int *userBalance, int *userWallet){
 
     //Creating deposit variable
     int depositAmount;
 
-    //Asking user for Dep Amount, then receiving input
+    //Asking user for DEP Amount, then receiving input
     printf("Enter deposit amount:  ");
     scanf("%d", &depositAmount);
 
-    //Adding amount to users balance, and printing new balance
-    *userBalance += depositAmount;
-    printf("Your new balance is: %d", *userBalance);
+    //Checking if user has the minimum amount to deposit, if not state insufficient funds
+    if(depositAmount <= *userWallet) {
+
+        //Adding DEP amount to users balance, and removing DEP amount from users wallet, then printing balances
+        *userBalance += depositAmount;
+        *userWallet -= depositAmount;
+        printf(" \n");
+        printf("Your new bank balance is: %d \n", *userBalance);
+        printf("Your wallet balance is now: %d", *userWallet);
+    } else {
+        printf(" \n");
+        printf("Insufficient Funds");
+    }
+
 
 }
 
 //Same as Deposit function except subtracting an amount from the users balance.
-void Withdrawal(int *userBalance){
+void Withdrawal(int *userBalance, int *userWallet){
 
     //Creating withdrawal amount
     int withdrawalAmount;
@@ -36,17 +47,33 @@ void Withdrawal(int *userBalance){
     printf("Enter withdrawal amount: ");
     scanf("%d", &withdrawalAmount);
 
-    //Removing amount from users balance, and printing new balance
-    *userBalance -= withdrawalAmount;
-    printf("Your new balance is: %d", *userBalance);
+    //Checking if user has the minimum amount to withdraw, if not state insufficient funds
+    if(withdrawalAmount <= *userBalance) {
+
+        //Removing WD amount from users balance, and adding WD amount to users wallet, then printing new balance
+        *userBalance -= withdrawalAmount;
+        *userWallet += withdrawalAmount;
+        printf(" \n");
+        printf("Your new bank balance is: %d \n", *userBalance);
+        printf("Your wallet balance is now: %d \n", *userWallet);
+    } else {
+        printf(" \n");
+        printf("Insufficient Funds");
+    }
 
 }
 
-//---------------------------
+//---------------------------------
 // Main function
-//---------------------------
+//--------------------------------
 
 int main (int argc, char *argv[]) {
+
+
+//--------------------------------
+// User Information
+//--------------------------------
+
 
 
     //Adding user login info
@@ -55,8 +82,9 @@ int main (int argc, char *argv[]) {
     int userID_input;
     int userPin_input;
 
-    //Creating user balance
+    //Creating user balances
     int userBalance = 10000;
+    int userWallet = 1840;
 
 
     //Welcoming User, asking for ID
@@ -74,20 +102,26 @@ int main (int argc, char *argv[]) {
         printf("User ID: %d \n", userID);
         printf("Enter your pin:");
         scanf("%d", &userPin_input);
+        printf(" \n");
 
-        //Checking if users pin input equals the correct  user pin, if so log user in and continue
+        //Checking if users pin input equals the correct user pin, if so log user in and continue
         if(userPin == userPin_input){
+            //Welcoming user, displaying balances
             printf("Welcome, user %d \n", userID);
-            printf("Your current balance is %d: \n", userBalance);
+            printf(" \n");
+            printf("Your current bank balance is: %d \n", userBalance);
+            printf("Your current wallet balance is: %d \n", userWallet);
 
             //Stating PIN is invalid, and exiting
         } else {
+            printf(" \n");
             printf("Invalid PIN");
             return 0;
         }
 
     //Stating id is invalid
     } else {
+        printf(" \n");
         printf("Invalid ID");
         return 0;
     }
@@ -108,7 +142,8 @@ int main (int argc, char *argv[]) {
 
 
     //Asking the user what they would like to do, then receiving input
-    printf("Would you like to Withdrawal, Deposit or Exit? \n");
+    printf(" \n");
+    printf("Would you like to Withdrawal (W), Deposit (D) or Exit? (E) \n");
 
     if (fgets(userAction, sizeof(userAction), stdin) != NULL) {
         //Removing newline character
@@ -119,18 +154,19 @@ int main (int argc, char *argv[]) {
     }
 
     //If userAction is Deposit, call Deposit function, else call Withdrawal Function
-    if (strcmp(userAction, "Deposit") == 0) {
-        Deposit(&userBalance);
+    if (strcmp(userAction, "D") == 0) {
+        Deposit(&userBalance, &userWallet);
 
-    } else if (strcmp(userAction, "Withdrawal") == 0) {
-        Withdrawal(&userBalance);
+    } else if (strcmp(userAction, "W") == 0) {
+        Withdrawal(&userBalance, &userWallet);
 
-    } else if (strcmp(userAction, "Exit") == 0){
+    } else if (strcmp(userAction, "E") == 0){
         printf(":Exiting");
         return 0;
 
 
     } else {
+        printf(" \n");
         printf("Invalid Option");
         return 1;
     }
